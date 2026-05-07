caf6b03f508ea4f2d6e38f4ab25c47f2b060fba2
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-05-07 15:10:39 +0800

    Implement Operations child class with inheritance and math behaviors

--- calculator_app.py
+++ calculator_app.py
@@ -2,5 +2,22 @@ class CalculatorBase:
     def __init__(self, num1, num1):
         self.num1 = num1
         self.num2 = num2
-        
-    
+
+class Operations(CalculatorBase):
+    def __init__ (self, num1, num2, operation):
+        super(). __init__(num1, num2)
+        self.operation = operation.strip()
+
+    def operations(self):
+        if self.operation == "+":
+            return self.num1 + self.num2
+        elif self.operation == "-":
+            return self.num1 - self.num2
+        elif self.operation == "*":
+            return self.num1 * self.num2
+        elif self.operation == "/"
+            return self.num1 / self.num2
+        if self.num2 == 0:
+            raise ValueDivisionError("Cannot divide by zero!")
+        return self.num1 / self.num2
+    else: return "Invalid operation!"
